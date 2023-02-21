211379444918053867543ec11c62069dd267374c
marciodesenvolvedor <89565704+marciodesenvolvedor@users.noreply.github.com>
2023-02-20 21:40:13 -0300

    Criação do Projeto

---
+++ portifólio1/src/portifólio1/PrimeiraClasse.java
@@ -0,0 +1,11 @@
+package portifólio1;
+
+public class PrimeiraClasse {
+	/*main é o metodo auto executavel em java*/
+	public static void main(String[] args) {
+	/*para salvar o projeto atalhos sempre ctrl + S */
+	/*para auto completar ctlr + espaço que ele vai completar o comando que você vai usar */
+		System.out.println("Meu primeiro programa em Java");
+	}
+
+}
